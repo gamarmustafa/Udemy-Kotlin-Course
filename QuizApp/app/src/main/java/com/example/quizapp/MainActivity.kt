a18c03f2162c6c5c8f7220587085d702f8d7f647
Gamar Mustafa <gamarmustafa@mail.ru>
2022-08-02 10:35:54 +0400

    add question interface

--- QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart: Button = findViewById(R.id.btn_start)
+        val etName: EditText = findViewById(R.id.et_name)  //editTextName
+        btnStart.setOnClickListener {
+            if (etName.text.isEmpty()){
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this, QuizQuestionsActivity::class.java)  // to go to the next activity
+                startActivity(intent)
+                finish() // so you cannot go back to the previous screen/activity
+            }
+        }
     }
 }
