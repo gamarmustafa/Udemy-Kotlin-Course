c4da4c7f39ca0d3246332f9e6e0544867d1cdf48
Gamar Mustafa <gamarmustafa@mail.ru>
2022-08-06 19:09:00 +0400

    add back button custom dialog

--- 7MinWorkoutApp/app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
+++ 7MinWorkoutApp/app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a7minutesworkout
 
+import android.app.Dialog
 import android.content.Intent
 import android.media.MediaPlayer
 import android.net.Uri
@@ -12,6 +13,7 @@ import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.a7minutesworkout.databinding.ActivityExerciseBinding
+import com.example.a7minutesworkout.databinding.DialogCutomBackConfirmationBinding
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -26,11 +28,11 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private var binding: ActivityExerciseBinding? = null
 
-    private var restTimerDuration: Long = 1
+    private var restTimerDuration: Long = 10
     private var restTimer: CountDownTimer? = null
     private var restProgress = 0
 
-    private var exerciseTimerDuration: Long = 1
+    private var exerciseTimerDuration: Long = 30
     private var exerciseTimer: CountDownTimer? = null
     private var exerciseProgress = 0
 
@@ -54,7 +56,7 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
         binding?.toolbarExercise?.setNavigationOnClickListener {
-            onBackPressed()
+            customDialogForBackButton()
         }
         exerciseList = Constants.defaultExerciseList()
 
@@ -62,6 +64,32 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         setupRestView()
         setupExerciseStatusRecyclerView()
     }
+
+    override fun onBackPressed() {
+        customDialogForBackButton()
+
+    }
+    private fun customDialogForBackButton(){
+        val customDialog = Dialog(this)
+        val dialogBinding = DialogCutomBackConfirmationBinding.inflate(layoutInflater)
+        customDialog.setContentView(dialogBinding.root)
+        customDialog.setCanceledOnTouchOutside(false)
+        dialogBinding.btnYes.setOnClickListener{
+            this@ExerciseActivity.finish()
+            customDialog.dismiss()
+
+        }
+        dialogBinding.btnNo.setOnClickListener{
+            customDialog.dismiss()
+        }
+
+        customDialog.show()
+
+    }
+
+
+
+
 
     private fun setupExerciseStatusRecyclerView(){
         binding?.rvExerciseStatus?.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
