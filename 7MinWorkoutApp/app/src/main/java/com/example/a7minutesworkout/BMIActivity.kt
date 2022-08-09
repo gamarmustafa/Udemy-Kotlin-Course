aa6bbf60722384f9f29cb43154511564a0ef68ab
Gamar Mustafa <gamarmustafa@mail.ru>
2022-08-09 12:12:16 +0400

    add imperial units for BMI calculator

--- 7MinWorkoutApp/app/src/main/java/com/example/a7minutesworkout/BMIActivity.kt
+++ 7MinWorkoutApp/app/src/main/java/com/example/a7minutesworkout/BMIActivity.kt
@@ -10,7 +10,16 @@ import java.math.BigDecimal
 import java.math.RoundingMode
 
 class BMIActivity : AppCompatActivity() {
+
+    companion object {
+        private const val METRIC_UNITS_VIEW = "METRIC_UNIT_VIEW"
+        private const val IMPERIAL_UNITS_VIEW = "IMPERIAL_UNIT_VIEW"
+    }
+
+    private var currentVisibleView: String = METRIC_UNITS_VIEW
+
     private var binding: ActivityBmiBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBmiBinding.inflate(layoutInflater)
@@ -25,6 +34,56 @@ class BMIActivity : AppCompatActivity() {
             onBackPressed()
         }
         binding?.btnCalculateUnits?.setOnClickListener {
+            calculateUnits()
+        }
+
+        makeVisibleMetricUnitsView()
+
+        binding?.rgUnits?.setOnCheckedChangeListener { _, checkedId: Int ->
+
+            if (checkedId == R.id.rbMetricUnits) {
+                makeVisibleMetricUnitsView()
+            } else {
+                makeVisibleImperialUnitsView()
+            }
+        }
+    }
+
+    private fun makeVisibleMetricUnitsView() {
+        currentVisibleView = METRIC_UNITS_VIEW
+
+        binding?.tilMetricUnitWeight?.visibility = View.VISIBLE
+        binding?.tilMetricUnitHeight?.visibility = View.VISIBLE
+        binding?.tilImperialFeet?.visibility = View.GONE
+        binding?.tilImperialInch?.visibility = View.GONE
+        binding?.tilImperialUnitWeight?.visibility = View.GONE
+
+        binding?.etMetricUnitHeight?.text!!.clear() //value is cleared if it is added
+        binding?.etMetricUnitWeight?.text!!.clear()
+
+        binding?.llDisplayBMIResult?.visibility = View.INVISIBLE
+
+    }
+
+    private fun makeVisibleImperialUnitsView() {
+        currentVisibleView = IMPERIAL_UNITS_VIEW
+
+        binding?.tilMetricUnitWeight?.visibility = View.INVISIBLE
+        binding?.tilMetricUnitHeight?.visibility = View.INVISIBLE
+        binding?.tilImperialFeet?.visibility = View.VISIBLE
+        binding?.tilImperialInch?.visibility = View.VISIBLE
+        binding?.tilImperialUnitWeight?.visibility = View.VISIBLE
+
+        binding?.etImperialUnitWeight?.text!!.clear() //value is cleared if it is added
+        binding?.etImperialFeet?.text!!.clear()
+        binding?.etImperialInch?.text!!.clear()
+
+        binding?.llDisplayBMIResult?.visibility = View.INVISIBLE
+
+    }
+
+    private fun calculateUnits() {
+        if (currentVisibleView == METRIC_UNITS_VIEW) {
             if (validateMetricUnits()) {
                 val heightValue: Float =
                     binding?.etMetricUnitHeight?.text.toString()
@@ -39,8 +98,20 @@ class BMIActivity : AppCompatActivity() {
                 Toast.makeText(this, "Please enter valid values.", Toast.LENGTH_SHORT).show()
             }
 
-        }
+        } else {
+            if (validateImperialUnits()) {
+                val feetValue: String = binding?.etImperialFeet?.text.toString()
+                val inchValue: String = binding?.etImperialInch?.text.toString()
+                val lbsValue: Float = binding?.etImperialUnitWeight?.text.toString().toFloat()
 
+                val heightValue = inchValue.toFloat() + feetValue.toFloat() * 12
+                val bmi = 703 * (lbsValue / (heightValue * heightValue))
+                displayBMIResult(bmi)
+
+            } else {
+                Toast.makeText(this, "Please enter valid values.", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
 
@@ -87,6 +158,22 @@ class BMIActivity : AppCompatActivity() {
             isValid = false
         } else if (binding?.etMetricUnitHeight?.text.toString().isEmpty()) {
             isValid = false
+        }
+        return isValid
+    }
+
+    private fun validateImperialUnits(): Boolean {
+        var isValid = true
+        when {
+            binding?.etImperialUnitWeight?.text.toString().isEmpty() -> {
+                isValid = false
+            }
+            binding?.etImperialFeet?.text.toString().isEmpty() -> {
+                isValid = false
+            }
+            binding?.etImperialInch?.text.toString().isEmpty() -> {
+                isValid = false
+            }
         }
         return isValid
     }
